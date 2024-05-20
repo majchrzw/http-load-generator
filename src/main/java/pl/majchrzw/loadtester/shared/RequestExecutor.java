6a3d3c8eb90ea477d90e2980ecdea2291d89e0dc
Dawid Szczepankowski <dawid.szczepankowski@elzab.pl>
2024-05-20 17:20:28 +0200

    Added randomized api calls

--- src/main/java/pl/majchrzw/loadtester/shared/RequestExecutor.java
+++ src/main/java/pl/majchrzw/loadtester/shared/RequestExecutor.java
@@ -3,10 +3,12 @@ package pl.majchrzw.loadtester.shared;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+import pl.majchrzw.loadtester.dto.config.NodeRequestConfig;
 import pl.majchrzw.loadtester.dto.statistics.NodeBundleExecutionStatistics;
 import pl.majchrzw.loadtester.dto.statistics.NodeExecutionStatistics;
 import pl.majchrzw.loadtester.dto.statistics.NodeSingleExecutionStatistics;
 import pl.majchrzw.loadtester.dto.config.RequestInfo;
+import pl.majchrzw.loadtester.dto.statistics.RequestIteratorData;
 
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -15,9 +17,7 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.time.Duration;
 import java.time.Instant;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 import java.util.concurrent.*;
 import java.util.stream.Collectors;
 
@@ -39,81 +39,81 @@ public class RequestExecutor {
 	
 	public void run() {
 		logger.info("Running http requests load");
-		List<NodeBundleExecutionStatistics> bundleExecutionStatistics = new ArrayList<>(dao.getRequestConfig().requests().size());
 		if (httpClient == null)
 			httpClient = HttpClient.newBuilder()
 					.executor(executorService)
 					.build();
-		
-		for (RequestInfo request : dao.getRequestConfig().requests()) {
-			NodeBundleExecutionStatistics bundle = this.executeRequestsBundle(request);
-			bundleExecutionStatistics.add(bundle);
+		Map<RequestInfo,List<NodeSingleExecutionStatistics>> requestsStatisticsMap = runAllRequests(dao.getRequestConfig());
+		List<NodeBundleExecutionStatistics> bundleExecutionStatistics = new ArrayList<>(dao.getRequestConfig().requests().size());
+		for (RequestInfo requestInfo : dao.getRequestConfig().requests()) {
+			List<NodeSingleExecutionStatistics> singleExecutionStatistics = requestsStatisticsMap.get(requestInfo);
+			NodeBundleExecutionStatistics bundleStatistics = new NodeBundleExecutionStatistics(singleExecutionStatistics,requestInfo);
+			bundleExecutionStatistics.add(bundleStatistics);
 		}
 		NodeExecutionStatistics statistics = new NodeExecutionStatistics(dao.getId(), bundleExecutionStatistics);
 		dao.setExecutionStatistics(statistics);
 	}
-	
-	private NodeBundleExecutionStatistics executeRequestsBundle(RequestInfo request) {
-		// TODO - trzeba dodać obsługę timeout-ów, bo teraz nie ma tego w ogóle
-		// TODO - request-y różnych typów powinny być chyba wysyłanie jednocześnie a nie sekwencyjnie
+
+	private Map<RequestInfo,List<NodeSingleExecutionStatistics>> runAllRequests(NodeRequestConfig requestConfig) {
+		Map<RequestInfo,List<CompletableFuture<NodeSingleExecutionStatistics>>> requestsStatisticsMapFuture = new HashMap<>();
+		for(RequestInfo requestInfo: requestConfig.requests()){
+			requestsStatisticsMapFuture.put(requestInfo, new ArrayList<>(requestInfo.count()));
+		}
+		Long seed = 2137L; //TODO - to trzeba zmienić na coś losowego
+		Long delayInMs = 50L; //TODO - mozna to wyciągnąć do konfiguracji
+		Iterator<RequestIteratorData> iterator = new RandomRequestIterator(dao.getRequestConfig(),seed);
+		ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+		Integer iteration = 0;
+		while(iterator.hasNext()){
+			final Integer finalIteration = iteration;
+			RequestIteratorData next = iterator.next();
+			scheduler.schedule(() -> {
+				CompletableFuture<NodeSingleExecutionStatistics> nodeSingleExecutionStatisticsCompletableFuture = handleAsyncRequest(next.request(), finalIteration);
+				requestsStatisticsMapFuture.get(next.requestInfo()).add(nodeSingleExecutionStatisticsCompletableFuture);
+			}, iteration * delayInMs, TimeUnit.MILLISECONDS);
+			iteration++;
+		}
+
+		scheduler.shutdown();
 		try {
-			Integer delayOfRequests = 10; // TODO - to będzie zmienna potem
-			ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
-			List<CompletableFuture<NodeSingleExecutionStatistics>> executionStatisticsFuture = new ArrayList<>(request.count());
-			HttpRequest httpRequest = this.prepareRequest(request);
-			for (int i = 0; i < request.count(); i++) {
-				int finalI = i;
-				scheduler.schedule(() -> {
-					CompletableFuture<NodeSingleExecutionStatistics> nodeSingleExecutionStatisticsCompletableFuture = handleAsyncRequest(httpRequest, finalI);
-					executionStatisticsFuture.add(nodeSingleExecutionStatisticsCompletableFuture);
-				}, i * delayOfRequests, TimeUnit.MILLISECONDS);
-			}
-			scheduler.shutdown();
-			try {
-				scheduler.awaitTermination(1, TimeUnit.HOURS);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-			CompletableFuture<Void> allOf = CompletableFuture.allOf(executionStatisticsFuture.toArray(new CompletableFuture[0]));
+			scheduler.awaitTermination(2, TimeUnit.HOURS);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		for (RequestInfo requestInfo : requestConfig.requests()) {
+			List<CompletableFuture<NodeSingleExecutionStatistics>> completableFutures = requestsStatisticsMapFuture.get(requestInfo);
+			CompletableFuture<Void> allOf = CompletableFuture.allOf(completableFutures.toArray(new CompletableFuture[0]));
 			try {
 				allOf.get();
 			} catch (InterruptedException | ExecutionException e) {
 				e.printStackTrace();
 			}
-			List<NodeSingleExecutionStatistics> executionStatistics = executionStatisticsFuture.stream().map(future -> {
+		}
+
+		Map<RequestInfo,List<NodeSingleExecutionStatistics>> requestsStatisticsMap = new HashMap<>();
+		for (RequestInfo requestInfo : requestConfig.requests()) {
+			List<NodeSingleExecutionStatistics> executionStatistics = requestsStatisticsMapFuture.get(requestInfo).stream().map(future -> {
 				try {
 					return future.get();
 				} catch (ExecutionException | InterruptedException e) {
 					return null;
 				}
 			}).filter(Objects::nonNull).collect(Collectors.toList());
-			
-			return new NodeBundleExecutionStatistics(executionStatistics, request);
-			
-		} catch (Exception e) {
-			//TODO dodac lepsza obsluge bledow
-			logger.error("Error while executing request: " + request.name(), e);
+			requestsStatisticsMap.put(requestInfo,executionStatistics);
 		}
-		return null;
+
+		return requestsStatisticsMap;
+
 	}
-	
-	private HttpRequest prepareRequest(RequestInfo requestInfo) throws URISyntaxException {
-		HttpRequest.BodyPublisher bodyPublisher = requestInfo.body() != null ? HttpRequest.BodyPublishers.ofString(requestInfo.body()) : HttpRequest.BodyPublishers.noBody();
-		//TODO dodac headers
-		return HttpRequest.newBuilder()
-				.uri(new URI(requestInfo.uri()))
-				.method(requestInfo.method().name(), bodyPublisher)
-				.timeout(Duration.ofMillis(requestInfo.timeout()))
-				.build();
-	}
-	
+
 	private CompletableFuture<NodeSingleExecutionStatistics> handleAsyncRequest(HttpRequest request, int i) {
 		Instant start = Instant.now();
 		return httpClient.sendAsync(request, HttpResponse.BodyHandlers.ofString()).thenApply(response -> {
 			System.out.println("Sending request: " + request);
 			Instant end = Instant.now();
 			Long elapsedTime = Duration.between(start, end).toMillis();
-			return new NodeSingleExecutionStatistics(i, elapsedTime, response.statusCode());
+			return new NodeSingleExecutionStatistics(i, elapsedTime, response.statusCode(),start);
 		});
 	}
 	

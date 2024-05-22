7a8d6c8a1ad6721edda383223553dad5c4a6e209
Wojciech <wojjmaj22@gmail.com>
2024-05-22 13:42:15 +0200

    changes time module

--- src/main/java/pl/majchrzw/loadtester/shared/config/MessageConverterConfiguration.java
+++ src/main/java/pl/majchrzw/loadtester/shared/config/MessageConverterConfiguration.java
@@ -2,7 +2,7 @@ package pl.majchrzw.loadtester.shared.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
-import com.fasterxml.jackson.datatype.jsr310.JSR310Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
@@ -17,7 +17,7 @@ public class MessageConverterConfiguration {
 		MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
 		ObjectMapper mapper = new ObjectMapper();
 		mapper.registerModule(new Jdk8Module());
-		mapper.registerModule(new JSR310Module());
+		mapper.registerModule(new JavaTimeModule());
 		converter.setObjectMapper(mapper);
 		converter.setTargetType(MessageType.TEXT);
 		converter.setTypeIdPropertyName("_type");
